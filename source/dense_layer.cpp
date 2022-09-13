cda349b41eab644c02d70c520f5c48f80cb77691
Yue Junfeng <yuejunfeng0909@gmail.com>
2022-09-13 12:29:51 +0800

    minor change

--- source/dense_layer.cpp
+++ source/dense_layer.cpp
@@ -10,20 +10,20 @@ void compute_dense(
 		DENSE_WEIGHTS_DTYPE dense_weights[DENSE_OUTPUT_NODES][DENSE_INPUT_NODES],
 		DENSE_BIAS_DTYPE dense_bias[DENSE_OUTPUT_NODES],
 		DENSE_OUTPUT_DTYPE dense_output[DENSE_OUTPUT_NODES]){
-#pragma HLS ARRAY_PARTITION variable=cnn_output_buffer type=cyclic dim=2 factor=2
-#pragma HLS ARRAY_PARTITION variable=dense_weights type=cyclic dim=2 factor=2
+#pragma HLS ARRAY_PARTITION variable=cnn_output_buffer type=cyclic dim=2 complete
+#pragma HLS ARRAY_PARTITION variable=dense_weights type=cyclic dim=2 complete
 #pragma HLS ARRAY_PARTITION variable=dense_bias type=complete
-#pragma HLS ARRAY_PARTITION variable=dense_output type=complete
 	// DENSE_OUTPUT_DTYPE pre_softmax_buffer[DENSE_OUTPUT_NODES];
 	CNN_OUT_DTYPE *cnn_output_buffer_alias = &cnn_output_buffer[0][0];
 
 	for (int i = 0; i < DENSE_OUTPUT_NODES; i++) {
+#pragma HLS pipeline
 
 		// for each output, calculate confidence
 		DENSE_OUTPUT_DTYPE output = 0;
 
 		for (int j = 0; j < DENSE_INPUT_NODES; j++) {
-#pragma HLS unroll
+#pragma HLS unroll factor = 11
 			output += cnn_output_buffer_alias[j] * dense_weights[i][j];
 		}
 
